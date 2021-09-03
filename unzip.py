49db432c870210f341a22a2c7bf361f3c2b68584
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-09-03 08:12:19 +0200

    Keep track of how many files we can unzip.

--- unzip.py
+++ unzip.py
@@ -17,12 +17,14 @@ def unzip(argv):
     # Only handle .zip files
     if zipped[-3:] != "zip":
         print(f"> {zipped} is not a zip file.")
+        print()
         return False
 
     # Unzip assignment file
     try:
         with zipfile.ZipFile(zipped, 'r') as read:
             read.extractall(destination)
+            print(f"> Unzipped {zipped}.")
     except:
         print("> Could not unzip assignment file.")
 
@@ -46,6 +48,10 @@ def unzip(argv):
         # We should still continue the routine
         print("> Could not remove .txt files, or no .txt files was found.")
 
+    # Keep track of how many files we can unzip.
+    count = 0
+    target = len(os.listdir())
+
     # For each zipped assignment in the destination directory
     for assignment in os.listdir():
         # Only handle .zip files
@@ -66,6 +72,8 @@ def unzip(argv):
         try:
             with zipfile.ZipFile(assignment, 'r') as read:
                 read.extractall(username)
+                count += 1
+                print(f"> Unzipped {username}'s deliverable. ({count} / {target})")
         except:
             print("> Could not unzip student's zipped assignment.")
             continue
@@ -85,7 +93,7 @@ if __name__ == '__main__':
     succeeded = unzip(sys.argv[1:])
 
     print(
-        "> Successfully unzipped file!"
+        "> Finished unzipping files!"
         if succeeded
-        else "> An error occurred when trying to unzip file!"
+        else "> An error occurred when trying to unzip files!"
     )
